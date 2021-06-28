8a3a63e37cf3543a58c858b7f875b8bef67bb42b
Alex Nguyen <45723364+alex-t-nguyen@users.noreply.github.com>
2021-06-28 17:12:02 -0500

    Final changes to program
    Fixed issue with filename and column values being different sizes

--- extract_data.py
+++ extract_data.py
@@ -12,8 +12,7 @@ import math
 def main():
     info_type = get_user_input_info()   # User input info type
     dir_path = get_user_input_dir() # User input directory path of root folder containing data files
-    #filename = 'Result Table.Result'
-    overwrite = get_user_input_overwrite()
+    overwrite = get_user_input_overwrite()  # User input for overwriting .csv files
 
     file_path_list = []
     df_list = []
@@ -36,24 +35,33 @@ def main():
             for path in file_path_list:
                 os.chdir(path)
                 df = get_data(constants.FILE_NAME, info_type)
-                #df['Data Set'] = path.split('\\')[-1]
-                df = add_identifier_columns(df, info_type, path.split('\\')[-1]).copy(deep=True)
-                #print(df)
-                new_filename = create_new_filename(path, constants.CSV_FILE_NAME)
-                df.to_csv(new_filename, sep = ',', index = False)
+                mask_df_list = create_mask_df(df)   # Create list of mask dataframes
+                
+                num_masks = len(mask_df_list)
+                for i in range(num_masks):
 
-                df['Limit Line'] = ''
+                    mask_df_list[i] = add_identifier_columns(mask_df_list[i], info_type, path.split('\\')[-1], num_masks, i).copy(deep=True)
+        
+                    new_filename = create_new_filename(path, constants.CSV_FILE_NAME, None, num_masks, i)
+                    
+                    mask_df_list[i].to_csv(new_filename, sep = ',', index = False)
 
-                df_list.append((df, path.split('\\')[-1]))
-                #print(df_list)
+                    if 'MT1' in mask_df_list[i].columns:    # If dataframe has MT column -> drop them
+                        for j in range(num_masks):                     
+                            mask_df_list[i].drop('MT{}'.format(j + 1), axis=1, inplace=True)
+                   
+                    mask_df_list[i]['Limit Line'] = ''  # Add limit line column and set values to empty
+                    
+                    df_list.append((mask_df_list[i], path.split('\\')[-1])) # Append individual dataframes to list
+                    
             
-            master_df = create_master_df(df_list)
+            master_df = create_master_df(df_list)   # Combine all dataframes into a master dataframe
 
-            master_df = add_limit_line(master_df, df_list, dir_path, info_type).copy(deep=True)
+            master_df = add_limit_line(master_df, df_list, dir_path, info_type).copy(deep=True) # Add limit line values to limit line dataframe in master dataframe
 
             os.chdir(dir_path)
-            aggregated_csv_filename = create_new_filename(dir_path, constants.AGGREGATED_CSV_FILE_NAME, constants.KEYS)
-            master_df.to_csv(aggregated_csv_filename, sep= ',', index = False)
+            aggregated_csv_filename = create_new_filename(dir_path, constants.AGGREGATED_CSV_FILE_NAME, constants.KEYS) 
+            master_df.to_csv(aggregated_csv_filename, sep= ',', index = False)  # Create .csv file from master dataframe
             
             if mode.lower() == constants.MODE_SELECTIVE:    # Mode is selective
                 print("\".csv\" files for all selected \".Result\" files successfullly created.")
@@ -122,92 +130,68 @@ def get_user_input_overwrite():
     return overwrite
 
 
-def create_new_filename(path, appended_filename, keys=None):
+def create_new_filename(path, appended_filename, keys=None, num_masks=1, mask_index=0):
     """
     Creates new file name for generated .csv file.\n
 
     :param path: path to directory of .Result file.\n
     :param appended_filename: name to append to folder name in the file name.\n
+    :param keys: keys in custom search to add to file name.\n
     :return: name of generated .csv file as string.
     """
     file_base_name = path.split('\\')[-1]
+    mask_name = ''
+    if num_masks > 1:
+        mask_name = '_M{}_'.format(mask_index + 1)
     if keys:
         keys_as_string = ' '.join(map(str, keys))
         string_keys = "_keys=[{}]".format(keys_as_string if keys else "")
-        return file_base_name + string_keys + appended_filename
+        return file_base_name + string_keys + mask_name + appended_filename
     else:
-        return file_base_name + appended_filename
+        return file_base_name + mask_name + appended_filename
 
 
 def create_master_df(df_list):
+    """
+    Creates a master dataframe that combines all of the data files and limit lines files in 1 dataframe.\n
+
+    :param df_list: list of dataframes to combine into a master dataframe
+    :return: dataframe containing all datafarmes in df_list
+    """
     master_dict = {}
-    master_df = pd.DataFrame() #df_list[0][0].copy(deep=True)
+    master_df = pd.DataFrame()
+
     # Check length of dataframe (# of rows) to determine size for new dictionary (master_dict)
     # Arrays in master_dict need to be same size to put into pandas dataframe
-    lf_index, largest_index_row = 0, len(df_list[0][0])#float(df_list[0][0]['Frequency'].iloc[-1])
+    lf_index, largest_index_row = 0, len(df_list[0][0])
     for i in range(1, len(df_list)):
-        index_row = len(df_list[i][0])#float(df_list[i][0]['Frequency'].iloc[-1])
+        index_row = len(df_list[i][0])
         if index_row > largest_index_row:
             lf_index, largest_index_row = i, index_row
 
-    #master_dict['Frequency'] = df_list[lf_index][0]['Frequency'].to_list() -------------------- Probably not used, so can delete --------------------------------------------------
-    """
-    for col in df_list[0][0].columns:
-        if col != 'Frequency':
-            master_df.drop(col, axis=1, inplace=True)   # Drop the data columns in master_df to prevent duplicates when comparing with the same data in loop below
-    """
-   # master_df['Frequency'] = pd.to_numeric(master_df['Frequency'], errors='coerce')
     for df in df_list:
-        temp_names = []
-        col_names = list(df[0].columns)
-        col_names.remove('Frequency')
-        for i in range(0, len(col_names)):
-            temp_names.append(df[1] + "_" + str(col_names[i])) # Makes file name (file_path + _ + col_name)
-        temp_names.insert(0, 'Frequency')
-        #print(temp_names)
-        #df[0].columns = temp_names # --------------- Uncomment if column names need to be different -----------------------------
-        
-        #df[0]['Frequency'] = pd.to_numeric(df[0]['Frequency'], errors='coerce')
-        #master_df = pd.merge(master_df, df[0], how='outer', on='Frequency')
         master_df = master_df.append(df[0], ignore_index=True, sort=False).copy(deep=True)
     master_df['Frequency'] = pd.to_numeric(master_df['Frequency'], errors='coerce').copy(deep=True)
-    #master_df = master_df.sort_values('Frequency', ascending=True) # ---------------------- Uncomment if frequency values need to be sorted ----------------------------------
-    #print(master_df)
-    """
-    for df in df_list:
-        temp_dict = df[0].to_dict('list')
-        del temp_dict["Frequency"]
-        old_key = temp_dict.keys()[0]
-        new_key = df[1] + "_" + old_key
-        temp_dict[new_key] = temp_dict[old_key]
-        del temp_dict[old_key]
 
-        #print(len(master_dict['Frequency']))
-        #print(len(temp_dict[new_key]))
-
-        for i in range(0, len(master_dict['Frequency']) - len(temp_dict[new_key])):
-            temp_dict[new_key].append("NaN")
-
-        master_dict.update(temp_dict)
-
-    master_df = pd.DataFrame(master_dict)
-    """
     return master_df
 
 
-def add_identifier_columns(data_frame, info_type, folder_name):
+def add_identifier_columns(data_frame, info_type, folder_name, num_masks, mask_index = 0):
     keys = folder_name.split(' ')
-    data_frame['Data Set'] = folder_name
-    if info_type.lower() == constants.TYPE_EMISSION:
+    if num_masks > 1:
+        data_frame['Data Set'] = folder_name + "_M{}".format(mask_index + 1)
+    else:
+        data_frame['Data Set'] = folder_name
+    if info_type.lower() == constants.TYPE_EMISSION:    # Add identifier columns for emission data
         for i in range(len(constants.EMISSION_IDENTIFIERS)):
-            if constants.EMISSION_IDENTIFIERS[i] == '-':
+            if constants.EMISSION_IDENTIFIERS[i] == '-':    # If column header is '-' -> don't add as column
                 continue
-            data_frame[constants.EMISSION_IDENTIFIERS[i]] = keys[i] if keys[i] != '-' else ''
-    elif info_type.lower() == constants.TYPE_DPI:
+            data_frame[constants.EMISSION_IDENTIFIERS[i]] = keys[i] if i < len(keys) and keys[i] != '-' else ''
+    elif info_type.lower() == constants.TYPE_DPI:   # Add identifier columns for dpi data
         for i in range(len(constants.DPI_IDENTIFIERS)):
-            if constants.DPI_IDENTIFIERS[i] == '-':
+            if constants.DPI_IDENTIFIERS[i] == '-': # If column header is '-' -> don't add as column
                 continue
-            data_frame[constants.DPI_IDENTIFIERS[i]] = keys[i] if keys[i] != '-' else ''
+            data_frame[constants.DPI_IDENTIFIERS[i]] = keys[i] if i < len(keys) and keys[i] != '-' else ''
 
     return data_frame
 
@@ -219,6 +203,8 @@ def add_limit_line(master_df, df_list, path, info_type):
     for (root, dirs, files) in os.walk(path, topdown=True): # Walk through all folders in root_dir
         for filename in files:
             if filename.endswith(constants.LIMIT_LINE_FILE_SUFFIX): # if .LimitLine file is in folder
+                if filename == constants.IGNORE_LL_HEADER:  # Ignore TargetLevel.LimitLine file
+                    continue
                 limit_line_file_flag = True               
                 if filename not in temp_ll_file_list:
                     limit_line_file_list.append(os.path.join(root, filename))
@@ -226,40 +212,26 @@ def add_limit_line(master_df, df_list, path, info_type):
     if not limit_line_file_flag:
         print("No \".LimitLine\" files found in specified path ({}).".format(path))
 
-    #print(limit_line_file_list)
     df_only_list = []
-    for df in df_list:  # -------------- LIMIT LINE ALWAYS 1-1000, MAYBE JUST USE 1 DF's FREQUENCY VALUES FOR ALL OF THE LIMIT LINES ---------------------------------
+    for df in df_list:  # Get just the dataframes and put them into a list (for easier access)
         df_only_list.append(df[0])
 
-    constants.LL_FREQUENCY_LIST = create_ll_frequency([])
+    constants.LL_FREQUENCY_LIST = create_ll_frequency([])   # Create frequency values for limit line
     for limit_line_path in limit_line_file_list: # Create limit line df for each file in limit line list
-        #for df in df_only_list: # Clear unwanted column data in df to make it a limit_line df instead
         df = df_only_list[0].copy(deep=True)    # temp df for limit line df
         for col in df.columns:
             if col not in constants.IMPORTANT_COL_NAMES:    # Empty data in columns not necessary for limit line df
                 df[col] = ''
         
         df['Data Set'] = 'LL ' + os.path.splitext(limit_line_path.split('\\')[-1])[0] # Create name for data set (LL + filename w/o extension)
-        """
-        if info_type.lower() == constants.TYPE_EMISSION:
-            df['Frequency'] == df['Frequency'].replace(df.loc[:, 'Frequency'], constants.LL_FREQUENCY_LIST_EMISSION)    # Replace frequency values of df with preset frequency values 
-        elif info_type.lower() == constants.TYPE_DPI:
-            df['Frequency'] == df['Frequency'].replace(df.loc[:, 'Frequency'], constants.LL_FREQUENCY_LIST_DPI)
-        """
+       
         with codecs.open(limit_line_path, encoding='utf-16-le') as limit_line_file:   
             for line in limit_line_file:  # Skips lines until reading 'TableValues' line
                 if constants.DATA_HEADER.encode(encoding='utf-16-le') not in line.encode(encoding='utf-16-le'): # Check for 'TableValues' string in line 
                     continue
                 else:
                     break
-            """
-            if info_type == constants.TYPE_EMISSION:
-                for i in range(27): # skip lines in limit line file
-                    limit_line_file.readline()
-            if info_type == constants.TYPE_DPI:
-                for i in range(28):
-                    limit_line_file.readline()
-            """
+            
             limit_lines = limit_line_file.readlines() # Get all remaining lines in file
 
             curr_line = limit_lines[0]
@@ -281,40 +253,15 @@ def add_limit_line(master_df, df_list, path, info_type):
                 
                 ll_df.sort_values(by='Frequency', ascending=True)
                 temp_df = ll_df.loc[(ll_df.loc[:, 'Frequency'].apply(lambda x: float(x)) >= coord0[0]) & (ll_df.loc[:, 'Frequency'].apply(lambda x: float(x)) <= coord1[0]), :].copy(deep=True) # ------------ REMEMBER TO CONVERT NUMBERS IN DF FROM STRING TO FLOAT ------------------------------------
-                #print(coord0[0])
+           
                 temp_df.loc[:, 'Limit Line'] = temp_df.loc[:, 'Frequency'].apply(lambda x: calc_limit(slope, float(x), coord0, coord1)).copy(deep=True)
 
-                #print(temp_df)
                 temp_df_list.append(temp_df)
-                #print(temp_df)
-                """ ----------------------------------- TAKES EXTREMELY LONG TIME ( > 5 MINUTES) -------------------------------
-                for index, row in df.iterrows():
-                    
-                    print(float(row['Frequency']))
-                    print(coord0)
-                    if float(row['Frequency']) >= coord1[0]:
-                        if math.isnan(slope):
-                            df['Limit Line'][index + 1] = coord1[1]
-                            print("break")
-                            break
-                        else:
-                            df['Limit Line'][index] = coord1[1]
-                        break
-                    if float(row['Frequency']) < coord0[0]:
-                        continue
-                    else:
-                        #df['Limit Line'] = df['Frequency'].apply(lambda x: calc_limit(slope, x, coord0[0], coord1[0]) if x in range(coord0[0], coord1[0] + 1) else '', axis=1)
-                        if slope == 0:
-                            df['Limit Line'][index] = coord0[1]
-                        else:
-                            df['Limit Line'][index] = calc_limit(slope, float(row['Frequency']), coord0)
-                """
+                
             new_df = pd.concat(temp_df_list, ignore_index=True)
-            #print(new_df)
+        
             master_df = master_df.append(new_df, ignore_index=True, sort=False).copy(deep=True)
-            
-            #master_df = new_df.append(master_df, ignore_index=True, sort=False)
-            #print(ll_df['Frequency'])
+       
     return master_df
 
 def get_limit_line_slope(signal0, signal1):
@@ -384,19 +331,19 @@ def create_ll_frequency(LL_FREQUENCY_LIST):
     while frequency <= 1e3: # Max frequency is 1 GHz
         while frequency < 10:  # 0.15 - 10 MHz
             LL_FREQUENCY_LIST.append(frequency)
-            frequency = frequency + 0.25   # Increment in 250 kHz steps
+            frequency = frequency + 0.01   # Increment in 250 kHz steps
         while frequency >= 10 and frequency < 100:  # 10 - 100 MHz
             LL_FREQUENCY_LIST.append(frequency)
-            frequency = frequency + 1   # Increment in 1 MHz steps
+            frequency = frequency + .01   # Increment in 1 MHz steps
         while frequency >= 100 and frequency < 200: # 100 - 200 MHz
             LL_FREQUENCY_LIST.append(frequency)
-            frequency = frequency + 2   # Increment in 2 MHz steps
+            frequency = frequency + .01   # Increment in 2 MHz steps
         while frequency >= 200 and frequency < 400:  # 200 - 400 MHz
             LL_FREQUENCY_LIST.append(frequency)
-            frequency = frequency + 4   # Increment in 4 MHz steps
+            frequency = frequency + .01   # Increment in 4 MHz steps
         while frequency >= 400 and frequency <= 1e3: # 400 MHz - 1 GHz
             LL_FREQUENCY_LIST.append(frequency)
-            frequency = frequency + 10   # Increment in 10 MHz steps
+            frequency = frequency + .01   # Increment in 10 MHz steps
     return LL_FREQUENCY_LIST
 
 
@@ -418,6 +365,79 @@ def create_ll_df(df, ll_list, path):
     ll_df['Frequency'] = ll_list
     ll_df['Data Set'] = 'LL ' + os.path.splitext(path.split('\\')[-1])[0] # Create name for data set (LL + filename w/o extension)
     return ll_df
+
+
+def create_mask_df(df):
+    # 1) Turn DataFrame into dictionary
+    # 2) Read Frequency, Power, and MT columns
+    # 3) Create n dictionaries based on how many MT columns there are
+    # 4) If all MT are 0 -> follow power levels
+            # If MT1 = 1 and MT2 = 0 -> MT1 follows power level, MT2 stays at the previous value before changing
+            # If MT1 = 0 and MT2 = 1
+    # 5) Turn all dictionaries into DataFrames
+    # 6) Return list of DataFrames
+    main_dict = df.to_dict()
+
+    level_key = list(df.columns)[1]
+    level_list = main_dict[level_key]
+    df_list = []
+    if 'MT3' in main_dict.keys():   # If file has 3 masks
+        common_level = level_list[0]
+        m1_dict = df.copy(deep=True).to_dict()
+        m2_dict = df.copy(deep=True).to_dict()
+        m3_dict = df.copy(deep=True).to_dict()
+        for i in range (len(main_dict['Frequency'])):
+            if not main_dict['MT1'][i] or not main_dict['MT2'][i] or not main_dict['MT3'][i]: # Check if MT column has an empty value, and skip it if so
+                continue
+            m1 = float(main_dict['MT1'][i])
+            m2 = float(main_dict['MT2'][i])
+            m3 = float(main_dict['MT3'][i])
+            if m1 == 0 and m2 == 0:
+                common_level = level_list[i]
+            elif m1 == 1 and m2 == 0 and m3 == 0:
+                m2_dict[level_key][i] = common_level
+                m3_dict[level_key][i] = common_level
+            elif m2 == 1 and m1 == 0 and m3 == 0:
+                m1_dict[level_key][i] = common_level
+                m3_dict[level_key][i] = common_level
+            elif m3 == 1 and m1 == 0 and m2 == 0:
+                m2_dict[level_key][i] = common_level
+                m1_dict[level_key][i] = common_level
+        
+        m1_df = pd.DataFrame(m1_dict)
+        m2_df = pd.DataFrame(m2_dict)
+        m3_df = pd.DataFrame(m3_dict)
+        df_list.append(m1_df)
+        df_list.append(m2_df)
+        df_list.append(m3_df)
+    elif 'MT2' in main_dict.keys(): # If file has 2 masks
+        common_level = level_list[0]
+        m1_dict = df.copy(deep=True).to_dict()
+        m2_dict = df.copy(deep=True).to_dict()
+        
+        for i in range (len(main_dict['Frequency'])):
+            if not main_dict['MT1'][i] or not main_dict['MT2'][i]:  # Check if MT column has an empty value, and skip it if so
+                continue
+            try:
+                m1 = float(main_dict['MT1'][i])
+                m2 = float(main_dict['MT2'][i])
+            except ValueError, e:
+                print("Mask convert to float error: " + str(e) + "on line " + str(i))
+            
+            if m1 == 0 and m2 == 0:
+                common_level = level_list[i]
+            elif m1 == 1 and m2 == 0:
+                m2_dict[level_key][i] = common_level
+            elif m2 == 1 and m1 == 0:
+                m1_dict[level_key][i] = common_level
+        m1_df = pd.DataFrame(m1_dict)
+        m2_df = pd.DataFrame(m2_dict)
+        df_list.append(m1_df)
+        df_list.append(m2_df)
+    else:   # If file has no masks -> return the original dataframe
+        df_list.append(df)
+    return df_list
+    
 
 if __name__ == "__main__":
     main()
